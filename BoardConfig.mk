a3107eb670860b30b13eb1ca16905563149c6a39
Phapoom Saksri <hello@mindhas403.dev>
2026-08-11 19:51:47 +0700

    re-enable sec_qc drivers; rebuild module lists (438/135)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,7 +75,8 @@ BOARD_BOOTCONFIG := \
     hardware=qcom \
     androidboot.memcg=1 \
     androidboot.usbcontroller=a600000.dwc3 \
-    androidboot.selinux=permissive
+    androidboot.selinux=permissive \
+    androidboot.init_fatal_panic=true
 
 BOARD_BOOT_HEADER_VERSION := 4
 BOARD_KERNEL_BASE := 0x00000000
